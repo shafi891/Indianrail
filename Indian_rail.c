b803f2b61b223516ae6e184bea31bf42dbcab065
shafiul alam <54498039+shafi891@users.noreply.github.com>
2020-03-23 16:43:59 +0530

    Update Indian_rail.c

--- Indian_rail.c
+++ Indian_rail.c
@@ -5,60 +5,98 @@
 #include<sys/wait.h>
 #include<unistd.h>
 #include<semaphore.h>
- int trainarrived= 0, allPassengerseated= 0, leftover= 0, seatingcounter= 0;
+ int train_arrived= 0, allPassenger_seated= 0, remains= 0, seating_counter= 0;
 
-struct Station
+struct Rail_Station
  {
- int NoOfseatsleft, waitingPassenger, SeatedPassenger,
+ int No_Of_seatsleft, waiting_Passenger, Seated_Passenger,
       sem_t lock;
  }S;
  
-     void station_init()
+     void Rail_station_init()
   {
-      seatingcounter= 0;
-         leftover= 0;
+      seating_counter= 0;
+         remains= 0;
       printf("\n ENTER THE TRAIN'S CAPACITY : ");
-          scanf("%d", &s.NoOfseatsleft);
-        if(S.NoOfseatsleft == 0)
+          scanf("%d", &S.No_Of_seatsleft);
+        if(S.No_Of_seatsleft == 0)
      {
         printf(" TRAIN IS ALREADY FULL>>> \n ");
         exit(0);
       }
- if(S.NoOfseatsleft < 0)
+ if(S.No_Of_seatsleft < 0)
       {
                printf(" SEATS CAN'T BE NEGATIVE \n");
                exit(0);
                
        }            
-   printf("ENTER THE NO OF PASSENGER THAT ARE WILLING TO BOARD AT STATION :");
-              scanf("%d", &S.waitingPassenger);
-   if(s.waitingPassenger<0)
+   printf("ENTER THE NO. OF PASSENGER THAT ARE WILLING TO BOARD AT STATION :");
+              scanf("%d", &S.waiting_Passenger);
+   if(S.waiting_Passenger<0)
       {
               printf("PASSENGER CAN'T BE NEGATIVE \N ");
        exit(0);
       }
       
-         if (s.waitingPassenger > s.NoOfseatsleft)
+         if (S.waiting_Passenger > S.No_Of_seatsleft)
   {
-                   leftover = S.waitingPassenger - S.NoOfseatsleft ;
-   printf("\n\n %d PASSENGER WILL BE LEFT AT STATION .\N\N",leftover);
+                   remains = S.waiting_Passenger - S.No_Of_seatsleft ;
+   printf("\n\n %d PASSENGER LEFT AT STATION .\N\N",remains);
+          S.waiting_Passenger = S.No_Of_seatsleft ;
    
    
   }
     
       
-      void* station_load_train(void *args)
+      void* Rail_station_train_load(void *args)
       {
-        struct station *p = (struct station *)args;
-       if((trainarrived == 1) && (p -> NoOfseatsleft > 0))
-       {
-        station_on_board(p);
-       }
-   }
+       
+         printf("NOW TRAIN IS IN STATION AND IT'S OPEN THE DOORS....);
+       
+               struct Rail_station *plt = (struct Rail_station *)args;
+                train_arrived = 1;
+                sleep(8);
+                
+                  if(plt-> No_Of_seatsleft<0){
+                            printf("\n SEATS LEFT: 0 \n");}
+                if(allPassenger_seated == 1 || plt-> No_Of_seatsleft== 1)
+                {
+               printf("\n SEATS LEFT: %d \n", plt-> No_Of_seatsleft)
+                printf("\n TRAIN DEPARTED...\n");
+                }
+                if(remains > 0)
+                {
+                    prointf("\n REMAINS PASSENGERS =%d \n, remaind);
+                            }
+                     } 
+       void Rail_station_board(struct station *stc)
+            { seating_counter ++;
+                  sem_wait(&s.lock);
+                allPassenger_seated= 0;
+                   stc-> No_Of_seatsleft--;
+                printf("%d SUCCESSFULLY PASSENGERS ARE  SEATED \n",seating_counter);
+                     allPassenger_seated= 1;
+             sem_post(&S.lock,0,1);
+             }
+                         
+       void* Rail_station_wait_for_train(void *args)
+                            {
+                 struct Rail_station *plt = (struct Rail_station * args);
+                  if((train_arrived ==1) &&(p-> No_Of_seatsleft<0))
+                  {
+                   Rail_station_board(plt);
+                  }
+                            }
+                
+                
+                             
       
      int main()
      {
-      int i = 0; array = 0;
-      printf("\t\t\t\t\t IND
-     
+      int j = 0; array = 0;
+           printf("\t\t!!!...INDIAN~TRAIN~IS~BOOTING...!!! \n");
+         
+                for(j=0; j<8; j++)
+                { 
+                     printf(".-.-.-.-.-.-.-")
    
